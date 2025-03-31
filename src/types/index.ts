af4fec379b094acd3b00869a84adcbc09e744656
bbhtruonglm <truonglm.bbh@gmail.com>
2025-03-31 18:07:39 +0700

    update

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,16 +1,29 @@
+/** Thông tin Ảnh */
 type PictureData = {
+  /** Chiều cao */
   height: number;
+  /** Dạng Ảnh */
   is_silhouette: boolean;
+  /**
+   * Link
+   */
   url: string;
+  /** Độ rông */
   width: number;
 };
 
+/** Kiểu dữ liệu ảnh */
 type Picture = {
   data: PictureData;
 };
-
+/** THông tin page */
 export type UserProfile = {
+  /**
+   * Id
+   */
   id: string;
+  /** Tên page */
   name: string;
+  /** ảnh */
   picture: Picture;
 };
