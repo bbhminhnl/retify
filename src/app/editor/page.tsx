49e4165d41e181e61737857c5463e4038290cc9c
bbhtruonglm <truonglm.bbh@gmail.com>
2025-05-06 16:20:53 +0700

    update

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,13 +1,21 @@
-import Tiptap from "@/components/Rich-Text-Editor/Tiptap";
+// import Tiptap from "@/components/Rich-Text-Editor/Tiptap";
 
-export default function EditorPage() {
-  return (
-    <div className="md:container md:mx-auto flex flex-col gap-y-4 flex-grow min-h-0 h-full justify-center items-center w-full pt-4 px-2">
-      <h1 className="text-2xl font-bold">Chỉnh sửa thông tin tài liệu</h1>
-      {/* <MarkdownEditor /> */}
-      <div className="overflow-hidden overflow-y-auto flex-grow min-h-0 w-full">
-        <Tiptap handleFinishEditor={() => {}} />
-      </div>
-    </div>
-  );
-}
+// export default function EditorPage() {
+//   return (
+//     <div className="md:container md:mx-auto flex flex-col gap-y-4 flex-grow min-h-0 h-full justify-center items-center w-full pt-4 px-2">
+//       <h1 className="text-2xl font-bold">Chỉnh sửa thông tin tài liệu</h1>
+//       {/* <MarkdownEditor /> */}
+//       <div className="overflow-hidden overflow-y-auto flex-grow min-h-0 w-full">
+//         <Tiptap handleFinishEditor={() => {}} />
+//       </div>
+//     </div>
+//   );
+// }
+
+import React from "react";
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
